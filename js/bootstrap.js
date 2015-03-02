aa689517e5e8ecfd6c34d4dbfcd6cafb881b6758
JonathanQu <jqdude@gmail.com>
2015-03-02 16:58:39 -0500

    Update bootstrap.js
    java script carousel

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2304,3 +2304,9 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
+
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('.carousel').carousel({interval: 7000});
+  });
+</script>
