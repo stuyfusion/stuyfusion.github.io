c24446672dffd3555c98f43127856b65e2865c4a
Kenneth Li <kennethli.3470@gmail.com>
2015-09-04 01:44:41 -0400

    Added some fancy effects to navbar and content in index.html.

--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,3 @@ function countdown(date, elementID, prefix, suffix) {
   var timeString = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
   document.getElementById(elementID).innerHTML = prefix + timeString + suffix;
 }
-
-var kickoff = new Date(2015, 8, 12, 12);
-setInterval(function() { countdown(kickoff, 'news-countdown', '<h1>2015 FIRST Tech Challenge Kickoff Countdown:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;', '</h1>') }, 1000);
