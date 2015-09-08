e63ad56b4538a0fd82b77874d1fe99705ca12dc7
Kenneth Li <kennethli.3470@gmail.com>
2015-09-07 21:32:56 -0400

    Styled gallery, changed header/footer to gradient scheme.

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,14 +1,17 @@
-function Gallery(divID, curPos) {
-  this.divID_ = divID;
-  this.parentEl_ = document.getElementById(this.divID_);
+function Gallery(parentId, galleryIdPrefix, curPos) {
+  this.galleryIdPrefix_ = galleryIdPrefix;
+  this.galleryEl_ = document.createElement('div');
+  this.galleryEl_.setAttribute('id', this.galleryIdPrefix_ + '-container');
+  this.parentId_ = parentId;
+  this.parentEl_ = document.getElementById(this.parentId_);
   this.curPos_ = curPos;
   this.images_ = [];
 }
 
-Gallery.prototype.addImage = function(imageID, imageClass, src) {
+Gallery.prototype.addImage = function(imageId, imageClass, src) {
   var imageEl_ = document.createElement('img');
-  if (imageID != null)
-    imageEl_.setAttribute('id', imageID);
+  if (imageId != null)
+    imageEl_.setAttribute('id', imageId);
   if (imageClass != null)
     imageEl_.setAttribute('class', imageClass);
   imageEl_.src = src;
@@ -20,33 +23,32 @@ Gallery.prototype.createButtons = function() {
   var prevBtn = document.createElement('input');
   var nextBtn = document.createElement('input');
 
-  prevBtn.setAttribute('id', this.divID_ + '-prev');
-  nextBtn.setAttribute('id', this.divID_ + '-next');
+  prevBtn.setAttribute('id', this.galleryIdPrefix_ + '-prev');
+  nextBtn.setAttribute('id', this.galleryIdPrefix_ + '-next');
   prevBtn.type = 'button';
   nextBtn.type = 'button';
   prevBtn.value = '<';
   nextBtn.value = '>'
   prevBtn.onclick = function() {
     gallery.curPos_ = (gallery.curPos_ - 1) % gallery.images_.length;
-    console.log(gallery.curPos_);
     gallery.update();
   };
   nextBtn.onclick = function() {
     gallery.curPos_ = (gallery.curPos_ + 1) % gallery.images_.length;
-    console.log(gallery.curPos_);
     gallery.update();
   };
 
-  this.parentEl_.appendChild(prevBtn);
-  this.parentEl_.appendChild(nextBtn);
+  this.galleryEl_.appendChild(prevBtn);
+  this.galleryEl_.appendChild(nextBtn);
 }
 
 Gallery.prototype.generate = function() {
   this.createButtons();
-  this.parentEl_.appendChild(this.images_[this.curPos_]);
+  this.galleryEl_.appendChild(this.images_[this.curPos_]);
+  this.parentEl_.appendChild(this.galleryEl_);
 }
 
 Gallery.prototype.update = function() {
-  this.parentEl_.removeChild(this.parentEl_.getElementsByTagName('img')[0]);
-  this.parentEl_.appendChild(this.images_[this.curPos_]);
+  this.galleryEl_.removeChild(this.parentEl_.getElementsByTagName('img')[0]);
+  this.galleryEl_.appendChild(this.images_[this.curPos_]);
 }
